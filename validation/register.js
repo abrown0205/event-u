893c4a1e84ffe26a47a9dbb85b13be0d0e3dd535
GabrielAC2019 <32580143+GabrielAC2019@users.noreply.github.com>
2021-07-06 17:51:39 -0400

    Add files via upload

---
+++ validation/register.js
@@ -0,0 +1,40 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+// Convert empty fields to an empty string so we can use validator functions
+  data.username = !isEmpty(data.username) ? data.username : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
+  data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.phone = !isEmpty(data.phone) ? data.phone : "";
+// Name checks
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "Username field is required";
+  }
+  if (Validator.isEmpty(data.firstName)) {
+    errors.firstName = "First name field is required";
+  }
+  if (Validator.isEmpty(data.lastName)) {
+    errors.lastName = "Last name field is required";
+  }
+// Email checks
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+// Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+// Phone checks
+  if (Validator.isEmpty(data.phone)) {
+    errors.phone = "Phone field is required";
+  }
+return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
