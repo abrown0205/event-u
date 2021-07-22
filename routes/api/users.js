f2d9b4099bada5cc9e79a91ddb67af14db5ac6dd
GabrielAC2019 <32580143+GabrielAC2019@users.noreply.github.com>
2021-07-22 18:17:27 -0400

    login returns active status as well

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,8 +11,6 @@ const { sendWelcomeEmail } = require("./welcome_emailer");
 
 const randomCode = Math.round(Math.random() * 999999);
 
-
-
 // Load input validation
 //const validateRegisterInput = require("../../validation/register");
 //const validateLoginInput = require("../../validation/login");
@@ -33,8 +31,8 @@ router.post("/register", async (req, res, next) => {
     password: req.body.password,
     email: req.body.email,
     notifications: true,
-	  active: false,
-	  activationCode: randomCode
+	active: false,
+	activationCode: randomCode
   });
 // Form validation
   //const { errors, isValid } = validateRegisterInput(req.body);
@@ -103,6 +101,7 @@ router.post("/login", async (req, res, next) => {
         var attendedEvents = user.attendedEvents;
         var likedEvents = user.likedEvents;
         var email = user.email;
+		var active = user.active;
 
         var ret;
         
@@ -113,6 +112,7 @@ router.post("/login", async (req, res, next) => {
         catch(e) {
           e = {error:e.message};
         }
+		ret["active"] = active;
         res.status(200).json(ret);
       } 
       else {
@@ -132,7 +132,7 @@ router.post("/preferences", async (req, res, next) => {
 
     var firstName = user.firstName;
     var lastName = user.lastName;
-    var userId = user._idƒ;
+    var userId = user._id;
     var uname = user.username;
     var attendedEvents = user.attendedEvents;
     var likedEvents = user.likedEvents;
@@ -150,68 +150,42 @@ router.post("/preferences", async (req, res, next) => {
   });
 });
 
-router.post("/likes", async (req, res, next) => {
-  const { username, likedEvents } = req.body;
+router.post("/activate", async (req, res, next) => {
+isActive = true;
+
+  const { username, activationCode} = req.body;
+  User.findOne({ username }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(400).json({ usernamenotfound: "User not found" }); 
+    }
+  if(activationCode != user.activationCode){
+	  isActive = false; 
+	  console.log(isActive);
+	  return res.status(400).json({ activationcodeincorrect: "Activation code incorrect" });
+  }
+
   let query = {username:username};
-  let update = {likedEvents:likedEvents};
+  let update = {active:isActive};
+  console.log(isActive);
   User.findOneAndUpdate(query, update).then(user => {
     // Check if user exists
     var ret;
+
     var firstName = user.firstName;
     var lastName = user.lastName;
     var userId = user._id;
     var uname = user.username;
     var attendedEvents = user.attendedEvents;
-    var preferences = user.preferences;
+    var likedEvents = user.likedEvents;
     var email = user.email;
 
-    try {
-      const token = require("../../createJWT.js");
-      ret = token.createToken( firstName, lastName, userId, uname, preferences, attendedEvents, likedEvents, email );
-    }
-    catch(e) {
-      console.log(e);
-    }
     
     res.status(200).json(ret);
   });
+
+  });
 });
 
-router.post("/activate", async (req, res, next) => {
-  isActive = true;
-  
-    const { username, activationCode} = req.body;
-    User.findOne({ username }).then(user => {
-      // Check if user exists
-      if (!user) {
-        return res.status(400).json({ usernamenotfound: "User not found" }); 
-      }
-    if(activationCode != user.activationCode){
-      isActive = false; 
-      console.log(isActive);
-      return res.status(400).json({ activationcodeincorrect: "Activation code incorrect" });
-    }
-  
-    let query = {username:username};
-    let update = {active:isActive};
-    console.log(isActive);
-    User.findOneAndUpdate(query, update).then(user => {
-      // Check if user exists
-      var ret;
-  
-      var firstName = user.firstName;
-      var lastName = user.lastName;
-      var userId = user._id;
-      var uname = user.username;
-      var attendedEvents = user.attendedEvents;
-      var likedEvents = user.likedEvents;
-      var email = user.email;
-  
-      
-      res.status(200).json(ret);
-    });
-  
-    });
-  });
 
 module.exports = router;
